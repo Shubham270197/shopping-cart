76202ffea92bb8a45da3a17e3cb8ca5f4bdc159e
shubham thakur <shubhamkharyal.akki@gmail.com>
2021-03-11 13:12:16 +0530

    new

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const { getAllProductById, getAllProducts} = require('../controller/productController')
+
+//@desc GET all products from database
+//@route GET /api/products
+//@access Public
+
+router.get('/', getAllProducts);
+
+//@desc GET a products by id from database
+//@route GET /api/products
+//@access Public
+
+router.get('/', getAllProductById);
+
+module.exports = router;
